1cdd612aa148ae2219bff399e67885ef527e6525
660e <solunar@vip.qq.com>
2023-02-22 23:20:05 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 
-createApp(App).use(router).mount('#app');
+import * as Cesium from 'cesium';
+
+const app = createApp(App);
+
+console.log(Cesium);
+
+app.use(router).mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,37 @@
 const { defineConfig } = require('@vue/cli-service');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = defineConfig({
+  configureWebpack: {
+    module: {
+      // unknownContextCritical: /^.\/.*$/,
+      // 基于vue的cesium安装及webpack配置
+      unknownContextCritical: false,
+      unknownContextRegExp: /\/cesium\/cesium\/Source\/Core\/buildModuleUrl\.js/
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: 'node_modules/cesium/Build/Cesium/Assets', to: 'Assets' },
+          { from: 'node_modules/cesium/Build/Cesium/ThirdParty', to: 'ThirdParty' },
+          { from: 'node_modules/cesium/Build/Cesium/Widgets', to: 'Widgets' },
+          { from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' }
+        ]
+      }),
+      new webpack.DefinePlugin({
+        CESIUM_BASE_URL: '/'
+      })
+    ],
+    resolve: {
+      fallback: {
+        http: require.resolve('stream-http'),
+        https: false,
+        url: false,
+        zlib: false
+      }
+    }
+  },
   devServer: {
     port: 3333,
     open: true
